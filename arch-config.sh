920563b445ba5f8fd372a53faae2e426d88aa4c6
axatbhardwaj <axatbhardwaj@outlook.com>
2023-04-24 16:11:34 +0530

    forarchImage docker

--- arch-config.sh
+++ arch-config.sh
@@ -1,34 +1,53 @@
 #!/bin/bash
+# This file is used to setup Fish
 
-#installing sudo 
-pacman -S sudo
+# Add sudo if not already running as root
+if [ "$EUID" -ne 0 ]
+  then sudo "$0" "$@"
+  exit
+fi
 
-#upgrading system
-sudo pacman -Syyu
+# Create a temporary user to run the installation
+useradd -m -s /bin/bash tempuser
+echo "tempuser ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
-#installing git
-sudo pacman -S git 
+# Install Fish
+sudo -u tempuser bash -c "yay -Sy fish --noconfirm"
+chsh -s $(which fish) tempuser
 
-#pacman base-devel
-sudo pacman -S --needed base-devel
+# Install Paru
+sudo -u tempuser bash -c "git clone https://aur.archlinux.org/paru.git && cd paru && makepkg -si --noconfirm"
+rm -rf /home/tempuser/paru
 
-#installing which 
-pacman -S which 
+# OMF installation
+sudo -u tempuser fish -c "curl -sL https://get.oh-my.fish | fish && omf install neolambda"
 
-#adding and switching to new users
-useradd  test
-passwd -d test
-su test
+# Fisher installation
+sudo -u tempuser fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
 
-#installing paru
-git clone https://aur.archlinux.org/paru.git
-cd paru
-sudo makepkg -si
-exit
+# Fisher extensions
+sudo -u tempuser fish -c "fisher install jorgebucaran/nvm.fish"
+sudo -u tempuser fish -c "fisher install jorgebucaran/replay.fish"
+sudo -u tempuser fish -c "fisher install franciscolourenco/done"
+sudo -u tempuser fish -c "fisher install gazorby/fish-abbreviation-tips"
+sudo -u tempuser fish -c "fisher install acomagu/fish-async-prompt"
+sudo -u tempuser fish -c "fisher install joseluisq/gitnow@2.11.0"
+sudo -u tempuser fish -c "fisher install meaningful-ooo/sponge"
 
-#deleting user 
-userdel test
+# Aliases
+sudo -u tempuser fish -c "alias dog \"code\"; funcsave dog;"
+sudo -u tempuser fish -c "alias dawg \"code-insiders\"; funcsave dawg;"
+sudo -u tempuser fish -c "alias lss \"ls -a -h\"; funcsave lss;"
+sudo -u tempuser fish -c "alias rmf \"rm -r -f\"; funcsave rmf;"
 
-#Installing fastfetch --arch
-paru -S fastfetch
+# Configuring launch options
+sudo -u tempuser bash -c "mkdir -p /home/tempuser/.config/fish"
+sudo -u tempuser bash -c "echo \"if status is-interactive
+  # Commands to run in interactive sessions can go here
+  fastfetch
+  export TERM=screen-256color
+end\" > /home/tempuser/.config/fish/config.fish"
 
+# Cleanup
+deluser tempuser
+rm /etc/sudoers.d/tempuser
