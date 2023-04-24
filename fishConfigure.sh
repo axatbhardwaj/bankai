472b9d90ce58186a47bcec22bcc2b418cae12c57
axatbhardwaj <axatbhardwaj@outlook.com>
2023-04-24 16:06:17 +0530

    final draft for Ubuntu

--- fishConfigure.sh
+++ fishConfigure.sh
@@ -1,49 +1,54 @@
-# #!/usr/bin/env fish
-# # This file is used to setup Fish
+#!/bin/bash
+# This file is used to setup Fish
 
-# #installing Fish
-# sudo passwd $USER -d #removing passwd for EC2 instance
+set -e  # Exit on error
 
-# sudo apt-add-repository ppa:fish-shell/release-3 
-# sudo apt-get update && sudo apt-get upgrade
-# sudo apt-get install fish
+# Remove password for EC2 instance
+sudo passwd $USER -d 
 
-#Making fish default shell 
+# Add Fish PPA and install Fish
+sudo add-apt-repository ppa:fish-shell/release-3    
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install fish
+
+# Make Fish the default shell 
 chsh -s $(which fish)
 
-# OMF-INSTALLATION
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install
-fish install --path=~/.local/share/omf --config=~/.config/omf
+# Install OMF
+curl -sL https://get.oh-my.fish | fish
 
-#Fisher- INSTALLATION
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+# Install OMF themes
+fish -c "omf install neolambda"
 
+# Install Fisher
+fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
 
-# Fisher extensions
-fisher install jorgebucaran/nvm.fish
-fisher install jorgebucaran/replay.fish
-fisher install franciscolourenco/done
-fisher install gazorby/fish-abbreviation-tips
-fisher install acomagu/fish-async-prompt
-fisher install joseluisq/gitnow@2.11.0
+# Install Fisher extensions
+fish -c "fisher install jorgebucaran/nvm.fish"
+fish -c "fisher install jorgebucaran/replay.fish"
+fish -c "fisher install franciscolourenco/done"
+fish -c "fisher install gazorby/fish-abbreviation-tips"
+fish -c "fisher install acomagu/fish-async-prompt"
+fish -c "fisher install joseluisq/gitnow@2.11.0"
+fish -c "fisher install meaningful-ooo/sponge"
 
-#OMF-THEMES
-omf install neolambda
+# Set aliases
+fish -c 'alias dog "code"; funcsave dog;' 
+fish -c 'alias dawg "code-insiders"; funcsave dawg;'
+fish -c 'alias lss "ls -a -h"; funcsave lss;'
+fish -c 'alias rmf "rm -r -f"; funcsave rmf;'
 
-#Aliasess
-alias dog "code"; funcsave dog; 
-alias dawg "code-insiders";funcsave dawg;
-alias lss "ls -ah";funcsave lss;
-alias rmf "rm -rf";funcsave rmf;
-
-#configuring launch options
-cd ~/.config/fish
-rmf config.fish 
+# Configure launch options
 printf "if status is-interactive
     # Commands to run in interactive sessions can go here
     fastfetch
     export TERM=screen-256color
-end" >> config.fish
-cd ~
+end\n" >> ~/.config/fish/config.fish
 
+# Install Fastfetch
+sudo add-apt-repository ppa:simonschneegans/testing
+sudo apt-get update
+sudo apt-get install fastfetch
 
+echo "Fish installation complete! Restart your terminal to start using Fish."
