b04d243802c3f23a8b7c5e485546b016f4327eb1
axatbhardwaj <axatbhardwaj@outlook.com>
2023-05-05 11:40:23 +0530

    borked

--- Arch-Docker.sh
+++ Arch-Docker.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
-# This file is used to setup Fish
+set -e # Exit on error
 
-set -e  # Exit on error
-
-#installing git 
-pacman -S git --noconfirm
-
-# Update system and install required packages
-# Install Paru if not already installed
-if ! command -v paru &> /dev/null
-then
-    echo "Paru not found. Installing Paru..."
-    git clone https://aur.archlinux.org/paru.git /tmp/paru
-    cd /tmp/paru
-    makepkg -si --noconfirm
+# Check if running as root
+if [[ $(id -u) -eq 0 ]]; then
+  echo "Please do not run this script as root."
+  exit 1
 fi
 
+# Update system and install required packages
+sudo pacman -Syu --noconfirm git 
+
+#installing fakeroot for makepkg
+sudo pacman -S --noconfirm base-devel alpm
+
+#updating 
+sudo pacman -Syu
 
 
-# Install Fish
-sudo paru -S fish
+
+# Install Paru if not already installed
+if ! command -v paru &> /dev/null; then
+  echo "Paru not found. Installing Paru..."
+  git clone https://aur.archlinux.org/paru.git /tmp/paru
+  cd /tmp/paru
+  sudo chown -R temp:temp $(pwd)
+  makepkg -si --noconfirm --nocheck
+
+  cd -
+fi
+
+# install Fish
+paru -Syyu --noconfirm fish
 
 # Make Fish the default shell 
 chsh -s $(which fish)
@@ -27,9 +38,6 @@ chsh -s $(which fish)
 # Install OMF
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install
 fish -c " fish install --path=~/.local/share/omf --config=~/.config/omf --noninteractive --yes"
-
-# Install OMF themes
-fish -c "omf install neolambda"
 
 # Install Fisher
 fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
@@ -44,19 +52,19 @@ fish -c "fisher install joseluisq/gitnow@2.11.0"
 fish -c "fisher install meaningful-ooo/sponge"
 
 # Set aliases
-fish -c 'alias dog "code"; funcsave dog;' 
+fish -c 'alias dog "code"; funcsave dog;'
 fish -c 'alias dawg "code-insiders"; funcsave dawg;'
 fish -c 'alias lss "ls -a -h"; funcsave lss;'
 fish -c 'alias rmf "rm -r -f"; funcsave rmf;'
 
 # Configure launch options
-printf "if status is-interactive
+echo 'if status is-interactive
     # Commands to run in interactive sessions can go here
     fastfetch
     export TERM=screen-256color
-end\n" >> ~/.config/fish/config.fish
+end' >> ~/.config/fish/config.fish
 
 # Install Fastfetch
-sudo pacman --noconfirm -Syu fastfetch
+sudo pacman -S --noconfirm fastfetch
 
 echo "Fish installation complete! Restart your terminal to start using Fish."
