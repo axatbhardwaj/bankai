7beadb3c8ea990aaed8d5426cea63e23e77523ce
Axat Bhardwaj <axatbhardwaj@outlook.com>
2023-11-09 12:25:13 +0530

    uncomment

--- nobara.sh
+++ nobara.sh
@@ -35,121 +35,121 @@ echo "notion-repackaged repository added to your package manager."
 sudo dnf install -y fish fastfetch code zip kitty thunderbird gh notion-app-enhanced
 
 
-# ####----------------------------------------- configuring flatpak and installing softwares ---------------------------------- ####
+####----------------------------------------- configuring flatpak and installing softwares ---------------------------------- ####
 
 
-# #Adding flathub repo 
-# flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+#Adding flathub repo 
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-# # Install Flatpak apps
-# flatpak install --user flathub io.github.mimbrero.WhatsAppDesktop -y
-# flatpak install --user flathub com.discordapp.Discord -y
-# flatpak install --user flathub com.github.IsmaelMartinez.teams_for_linux -y
-# flatpak install --user flathub com.spotify.Client -y
-# flatpak install --user flathub app/one.ablaze.floorp/x86_64/stable -y 
-# flatpak install --user flathub org.signal.Signal -y
-# flatpak install --user flathub com.authy.Authy -y
+# Install Flatpak apps
+flatpak install --user flathub io.github.mimbrero.WhatsAppDesktop -y
+flatpak install --user flathub com.discordapp.Discord -y
+flatpak install --user flathub com.github.IsmaelMartinez.teams_for_linux -y
+flatpak install --user flathub com.spotify.Client -y
+flatpak install --user flathub app/one.ablaze.floorp/x86_64/stable -y 
+flatpak install --user flathub org.signal.Signal -y
+flatpak install --user flathub com.authy.Authy -y
 
 
-# # Make Fish the default shell k
-# chsh -s $(which fish)
+# Make Fish the default shell k
+chsh -s $(which fish)
 
-# # Install OMF and themes
-# curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install
-# fish -c "fish install --path=~/.local/share/omf --config=~/.config/omf --noninteractive --yes"
-# fish -c "omf install neolambda"
+# Install OMF and themes
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install
+fish -c "fish install --path=~/.local/share/omf --config=~/.config/omf --noninteractive --yes"
+fish -c "omf install neolambda"
 
-# ####---------------------------------------------------- configuring fish------------------------------------------------------####
+####---------------------------------------------------- configuring fish------------------------------------------------------####
 
-# fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
-# fish -c "fisher install jorgebucaran/nvm.fish"
-# fish -c "fisher install jorgebucaran/replay.fish"
-# fish -c "fisher install franciscolourenco/done"
-# fish -c "fisher install gazorby/fish-abbreviation-tips"
-# fish -c "fisher install acomagu/fish-async-prompt"
-# fish -c "fisher install joseluisq/gitnow@2.11.0"
-# fish -c "fisher install meaningful-ooo/sponge"
+fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
+fish -c "fisher install jorgebucaran/nvm.fish"
+fish -c "fisher install jorgebucaran/replay.fish"
+fish -c "fisher install franciscolourenco/done"
+fish -c "fisher install gazorby/fish-abbreviation-tips"
+fish -c "fisher install acomagu/fish-async-prompt"
+fish -c "fisher install joseluisq/gitnow@2.11.0"
+fish -c "fisher install meaningful-ooo/sponge"
 
-# # Set aliases
-# fish -c 'alias dog "code"; funcsave dog;' 
-# fish -c 'alias lss "ls -a -h"; funcsave lss;'
-# fish -c 'alias rmf "rm -r -f"; funcsave rmf;'
-# fish -c 'alias ps "ps auxfh"; funcsave ps;' 
+# Set aliases
+fish -c 'alias dog "code"; funcsave dog;' 
+fish -c 'alias lss "ls -a -h"; funcsave lss;'
+fish -c 'alias rmf "rm -r -f"; funcsave rmf;'
+fish -c 'alias ps "ps auxfh"; funcsave ps;' 
 
-# ####-------------------------------------------------------- git config -------------------------------------------------------####
-# #creating directory for work and personal git
+####-------------------------------------------------------- git config -------------------------------------------------------####
+#creating directory for work and personal git
 
-# mkdir -p ~/work
-# cd ~/work
-# printf "# ~/work/.gitconfig.work
+mkdir -p ~/work
+cd ~/work
+printf "# ~/work/.gitconfig.work
  
-# [user]
-# email = {work_email}
-# name = {work_username}
+[user]
+email = {work_email}
+name = {work_username}
  
-# [github]
-# user = "{github_username}"
+[github]
+user = "{github_username}"
  
-# [core]
-# sshCommand = "ssh -i {ssh_key_path}"
-# " > ~/work/.gitconfig.work
+[core]
+sshCommand = "ssh -i {ssh_key_path}"
+" > ~/work/.gitconfig.work
 
 
-# mkdir -p ~/personal
-# cd ~/personal
-# printf "# ~/personal/.gitconfig.personal
+mkdir -p ~/personal
+cd ~/personal
+printf "# ~/personal/.gitconfig.personal
  
-# [user]
-# email = {personal_email}
-# name = {personal_username}
+[user]
+email = {personal_email}
+name = {personal_username}
  
-# [github]
-# user = "{github_username}"
+[github]
+user = "{github_username}"
  
-# [core]
-# sshCommand = "ssh -i {ssh_key_path}"
-# " > ~/personal/.gitconfig.personal
+[core]
+sshCommand = "ssh -i {ssh_key_path}"
+" > ~/personal/.gitconfig.personal
 
-# #creating a vscodeSsh config for remote ssh connections
-# printf "Host {HOSTNAME}
-# HostName
-# User 
-# IdentityFile " > ~/vsCodeSshConfig.config
+#creating a vscodeSsh config for remote ssh connections
+printf "Host {HOSTNAME}
+HostName
+User 
+IdentityFile " > ~/vsCodeSshConfig.config
 
-# #configuring global gitconfig
-# printf "# ~/.gitconfig
+#configuring global gitconfig
+printf "# ~/.gitconfig
 
-# [includeIf "gitdir:~/personal/"]
-#     path = ~/personal/.gitconfig.personal
+[includeIf "gitdir:~/personal/"]
+    path = ~/personal/.gitconfig.personal
 
-# [includeIf "gitdir:~/infrablok/"]
-#     path = ~/work/.gitconfig.work
+[includeIf "gitdir:~/infrablok/"]
+    path = ~/work/.gitconfig.work
 
-# [core]
-#     excludesfile = ~/.gitignore
-# "> ~/.gitconfig
-
-
-# ####----------------------------------------------- configuring kitty and Fish launch ---------------------------------------- ####
+[core]
+    excludesfile = ~/.gitignore
+"> ~/.gitconfig
 
 
-# # Configure kitty
-# config_file="$HOME/.config/kitty/kitty.conf"
-# if [ ! -f "$config_file" ]; then
-#     mkdir -p "$HOME/.config/kitty"
-#     touch "$config_file"
-# fi
-# printf "background_opacity 0.75\n" >> "$config_file"
-# printf "Transparency setting added to kitty.conf. Restart Kitty for the changes to take effect.\n"
+####----------------------------------------------- configuring kitty and Fish launch ---------------------------------------- ####
 
-# # Configure Fish launch options
-# printf "if status is-interactive
-#     fastfetch
-#     export TERM=screen-256color
-#     set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
-# end\n" > ~/.config/fish/config.fish
 
-# # Update system and install necessary packages
-# nobara-sync
+# Configure kitty
+config_file="$HOME/.config/kitty/kitty.conf"
+if [ ! -f "$config_file" ]; then
+    mkdir -p "$HOME/.config/kitty"
+    touch "$config_file"
+fi
+printf "background_opacity 0.75\n" >> "$config_file"
+printf "Transparency setting added to kitty.conf. Restart Kitty for the changes to take effect.\n"
 
-# echo "Nobara Linux setup complete! Restart your terminal to start using Fish."
+# Configure Fish launch options
+printf "if status is-interactive
+    fastfetch
+    export TERM=screen-256color
+    set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
+end\n" > ~/.config/fish/config.fish
+
+# Update system and install necessary packages
+nobara-sync
+
+echo "Nobara Linux setup complete! Restart your terminal to start using Fish."
